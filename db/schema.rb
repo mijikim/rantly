710136cd2f74460329f2fae9ccef9365359a8882
mijikim <miji.kim926@gmail.com>
2014-10-27 17:29:31 -0600

    removed avatar_url column that was created for aws implementation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141027041424) do
+ActiveRecord::Schema.define(version: 20141027232831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,7 +68,6 @@ ActiveRecord::Schema.define(version: 20141027041424) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.string   "avatar_url"
   end
 
 end
